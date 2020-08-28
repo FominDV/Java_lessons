c7e036fcc50b5212089a5753384a74f1f28ae3d0
Dmitriy Fomin <79067773397@yandex.ru>
2020-08-28 08:48:29 +0300

    Java1_homework1.4

--- homework1-1/src/main/java/ru/java_core_1/homework/fomin/FirstHomeWork.java
+++ homework1-1/src/main/java/ru/java_core_1/homework/fomin/FirstHomeWork.java
@@ -1,5 +1,8 @@
 package ru.java_core_1.homework.fomin;
 
+
+import java.util.Scanner;
+
 public class FirstHomeWork {
     public static void main(String args[]) {
         //initialization of primitive types
@@ -11,8 +14,16 @@ public class FirstHomeWork {
         double doubleNumber = 9.834;
         char symbol = 'F';
         boolean flag = true;
+        String line = "Hello World";
         System.out.println("a*(b+c/d)="+getCalculate(2.5,3L,10,floatNumber));
         System.out.println("the sum of numbers less than or equal to 20 and greater than 10: " + isCompareNumbers(7.6, floatNumber ));
+        setCompareZero(integerNumber);
+        System.out.println("number is negative: " + isNegative(47));
+        //Read name from console and method "setHello" call
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?\nInput your name: ");
+        String name = input.nextLine();
+        setHello(name);
     }
 
     //part 3
@@ -26,4 +37,19 @@ public class FirstHomeWork {
         return (sum > 10) && (sum <=20);
     }
     //part5
+    public static void setCompareZero(int number){
+        if(number>=0)
+            System.out.println(number + " is greater than or equal to zero");
+        else
+            System.out.println(number + " is less than zero");
+    }
+    //part6
+    public static boolean isNegative(int number){
+        return (number<0);
+    }
+    //part 7
+    public static void setHello(String name){
+        System.out.println("Привет, " + name);
+    }
+   
 }
