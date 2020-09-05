21f2b3ca8132e8ca9ab1e9fc4b496b1310c3a885
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-05 15:35:57 +0300

    Homework1-3

--- homework1-1/src/main/java/ru/java_core_1/homework/fomin/ThirdHomeWork.java
+++ homework1-1/src/main/java/ru/java_core_1/homework/fomin/ThirdHomeWork.java
@@ -22,10 +22,15 @@ import java.util.Scanner;
 
 public class ThirdHomeWork {
     static Scanner read = new Scanner(System.in);
+    static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+            "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+            "pepper", "pineapple", "pumpkin", "potato"};
 
     public static void main(String[] args) {
         //Task1
         guessTheNumberGame();
+        //Task 2
+        guessTheWordGame();
     }
 
     //Methods for task1
@@ -46,9 +51,8 @@ public class ThirdHomeWork {
         int randomNumber = generateRandomNumber(10);
         int playerNumber;
         for (int attemptNow = attempts; attemptNow > 0; attemptNow--) {
-            System.out.println("Now range of your attempt is " + attemptNow);
-            System.out.print("Inter your answer: ");
-            playerNumber = read.nextInt();
+            System.out.println("Now range of your attempts is " + attemptNow);
+            playerNumber = inputUserNumber();
             if (playerNumber < randomNumber) {
                 System.out.println(playerNumber + " is less than guess number");
             } else if (playerNumber > randomNumber) {
@@ -61,12 +65,68 @@ public class ThirdHomeWork {
         System.out.println("Sorry, you have not more attempts!\nThe guess number was " + randomNumber);
     }
 
+    static int inputUserNumber() {
+        int userNumber;
+
+        do {
+            System.out.println("Inter your answer: ");
+            userNumber = verifyInt();
+        } while (!(userNumber >= 0 && userNumber <= 9));
+        return userNumber;
+    }
+
+    static int verifyInt() {
+        String userInput;
+        boolean test;
+        int number = -1;
+        do {
+            test = read.hasNextInt();
+            userInput = read.next();
+            if (test) {
+                number = Integer.parseInt(userInput);
+            } else {
+                System.out.println("You did not input the integer number!\nTry gain");
+            }
+        } while (!(test));
+        return number;
+    }
+
     static int playerAnswer() {
         int answer = 2;
         do {
             System.out.println("If you want to play again - write 1\nFor exit write 0");
-            answer = read.nextInt();
+            answer = verifyInt();
         } while (answer != 0 && answer != 1);
         return answer;
+    }
+
+    //Methods for task2
+    static void guessTheWordGame() {
+        String userWord;
+        System.out.println("In this game you should guess the word!\nYou have infinity attempts!\nAfter each attempt you may get hint!\nLet's start!!!");
+        String guessWord = words[generateRandomNumber(words.length)];
+        while (true) {
+            System.out.println("Inter your word:");
+            userWord = read.next();
+            if (userWord.equalsIgnoreCase(guessWord)) {
+                System.out.println("You win! It's really " + guessWord);
+                break;
+            } else {
+                giveHint(guessWord, userWord);
+            }
+        }
+    }
+
+    static void giveHint(String guessWord, String userWord) {
+        userWord = userWord.toLowerCase();
+        System.out.print("It isn't right word!\nThe hint from your last answer: ");
+        for (int i = 0; i < 15; i++) {
+            if (i < userWord.length() && i < guessWord.length() && guessWord.charAt(i) == userWord.charAt(i)) {
+                System.out.print(guessWord.charAt(i));
+            } else {
+                System.out.print("#");
+            }
+        }
+        System.out.println();
     }
 }
