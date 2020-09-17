868bae0563d83cd850b46bad97bf6e6573f35062
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-17 22:24:07 +0300

    homework7.1

--- homework1-1/src/main/java/ru/java_core_1/homework7/fomin/Cat.java
+++ homework1-1/src/main/java/ru/java_core_1/homework7/fomin/Cat.java
@@ -1,4 +1,13 @@
 package ru.java_core_1.homework7.fomin;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    Cat(String name, int appetite){
+        this.name=name;
+        this.appetite=appetite;
+    }
+    protected void eat(Plate plate){
+
+    }
 }
