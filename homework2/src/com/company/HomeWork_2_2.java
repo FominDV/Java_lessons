b702ad82c51a4278117132acb8967a58422274e4
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-30 19:28:36 +0300

    new update 3

--- homework2/src/com/company/HomeWork_2_2.java
+++ homework2/src/com/company/HomeWork_2_2.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class HomeWork_2_2 extends ProcessingString {
+public class HomeWork_2_2 {
     private static final String LINE = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
     private static String userLine = "";
 
@@ -12,7 +12,7 @@ public class HomeWork_2_2 extends ProcessingString {
 
         System.out.println("Start line:\n" + LINE);
         try {
-            processingString(LINE);
+            ProcessingString.processingString(LINE);
         } catch (SizeOfStringArrayException e) {
             System.err.println(e.getMessage());
         } catch (ConvertStringArrayToIntArrayException e) {
@@ -25,7 +25,7 @@ public class HomeWork_2_2 extends ProcessingString {
             userLine += read.nextLine() + "\n";
         }
         try {
-            processingString(userLine);
+            ProcessingString.processingString(userLine);
         } catch (SizeOfStringArrayException e) {
             System.err.println(e.getMessage());
         } catch (ConvertStringArrayToIntArrayException e) {
