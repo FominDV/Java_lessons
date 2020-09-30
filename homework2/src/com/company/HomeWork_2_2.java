b93953a5fd39af978ee9a5f982084a9e2296d0f0
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-30 16:31:22 +0300

    add exceptions

--- homework2/src/com/company/HomeWork_2_2.java
+++ homework2/src/com/company/HomeWork_2_2.java
@@ -4,36 +4,59 @@ import java.util.Arrays;
 
 public class HomeWork_2_2 {
     private static final String LINE = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+    private static final String TEST_LINE = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1w 0";
 
     public static void main(String[] args) {
-        String[][] stringArr = stringDoubleArr(LINE);
+        System.out.println("Start line:\n" + TEST_LINE);
         System.out.println("String array[][]:");
+        try {
+            getStringDoubleArr(TEST_LINE);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Array's size isn't 4 by 4");
+        }
+        String[][] stringArr = getStringDoubleArr(TEST_LINE);
         for (String[] arr : stringArr) {
             System.out.print(Arrays.toString(arr) + "\n");
         }
-        int[][] intArr = convertStringArrToIntArr(stringArr);
         System.out.println("Integer array[][]:");
+        try {
+            convertStringArrToIntArr(stringArr);
+        }
+        catch (NumberFormatException e){
+            System.out.println("Some elements of string array are not integer numbers");
+        }
+        int[][] intArr = convertStringArrToIntArr(stringArr);
         for (int[] arr : intArr) {
             System.out.print(Arrays.toString(arr) + "\n");
         }
         System.out.println("Sum of elements integer array is " + getSumOfIntArr(intArr));
-        System.out.println("Sum of elements integer array is " + getDivision(getSumOfIntArr(intArr)));
+        System.out.println("Calculation of division is " + getDivision(getSumOfIntArr(intArr)));
     }
 
-    static String[][] stringDoubleArr(String line) {
+    static String[][] getStringDoubleArr(String line) throws IndexOutOfBoundsException {
         String[] stringFirstArr = line.split("\n");
-        String[][] stringSecondArr = new String[stringFirstArr.length][stringFirstArr.length];
+        String[][] stringSecondArr = new String[stringFirstArr.length][];
         for (int i = 0; i < stringFirstArr.length; i++) {
             stringSecondArr[i] = stringFirstArr[i].split(" ");
+        }
+        if (stringSecondArr.length != 4) throw new IndexOutOfBoundsException();
+        for (String[] singleStringArray : stringSecondArr) {
+            if (singleStringArray.length != 4) throw new IndexOutOfBoundsException();
         }
         return stringSecondArr;
     }
 
-    static int[][] convertStringArrToIntArr(String[][] stringArr) {
-        int[][] intArr = new int[stringArr.length][stringArr[0].length];
+    static int[][] convertStringArrToIntArr(String[][] stringArr) throws NumberFormatException {
+        int[][] intArr = new int[stringArr.length][];
         for (int i = 0; i < stringArr.length; i++) {
+            intArr[i] = new int[stringArr[i].length];
             for (int j = 0; j < stringArr[i].length; j++) {
-                intArr[i][j] = Integer.parseInt(stringArr[i][j]);
+                try {
+                    intArr[i][j] = Integer.parseInt(stringArr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw e;
+                }
             }
         }
         return intArr;
