c2b0bea400ef7eba70697c4aff26892d00dddeaa
Dmitriy Fomin <79067773397@yandex.ru>
2020-09-30 17:13:23 +0300

    add user line

--- homework2/src/com/company/HomeWork_2_2.java
+++ homework2/src/com/company/HomeWork_2_2.java
@@ -1,37 +1,22 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HomeWork_2_2 {
     private static final String LINE = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
-    private static final String TEST_LINE = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1w 0";
+    private static String userLine = "";
 
     public static void main(String[] args) {
-        System.out.println("Start line:\n" + TEST_LINE);
-        System.out.println("String array[][]:");
-        try {
-            getStringDoubleArr(TEST_LINE);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Start line:\n" + LINE);
+        processingString(LINE);
+        System.out.println("Enter string, between values must be spaces:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Enter string of four values number " + i);
+            userLine += read.nextLine() + "\n";
         }
-        catch (IndexOutOfBoundsException e){
-            System.out.println("Array's size isn't 4 by 4");
-        }
-        String[][] stringArr = getStringDoubleArr(TEST_LINE);
-        for (String[] arr : stringArr) {
-            System.out.print(Arrays.toString(arr) + "\n");
-        }
-        System.out.println("Integer array[][]:");
-        try {
-            convertStringArrToIntArr(stringArr);
-        }
-        catch (NumberFormatException e){
-            System.out.println("Some elements of string array are not integer numbers");
-        }
-        int[][] intArr = convertStringArrToIntArr(stringArr);
-        for (int[] arr : intArr) {
-            System.out.print(Arrays.toString(arr) + "\n");
-        }
-        System.out.println("Sum of elements integer array is " + getSumOfIntArr(intArr));
-        System.out.println("Calculation of division is " + getDivision(getSumOfIntArr(intArr)));
+        processingString(userLine);
     }
 
     static String[][] getStringDoubleArr(String line) throws IndexOutOfBoundsException {
@@ -74,5 +59,30 @@ public class HomeWork_2_2 {
 
     static double getDivision(int sum) {
         return sum / 2;
+    }
+
+    static void processingString(String line) {
+        System.out.println("String array[][]:");
+        try {
+            getStringDoubleArr(line);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Array's size isn't 4 by 4");
+        }
+        String[][] stringArr = getStringDoubleArr(line);
+        for (String[] arr : stringArr) {
+            System.out.print(Arrays.toString(arr) + "\n");
+        }
+        System.out.println("Integer array[][]:");
+        try {
+            convertStringArrToIntArr(stringArr);
+        } catch (NumberFormatException e) {
+            System.out.println("Some elements of string array are not integer numbers");
+        }
+        int[][] intArr = convertStringArrToIntArr(stringArr);
+        for (int[] arr : intArr) {
+            System.out.print(Arrays.toString(arr) + "\n");
+        }
+        System.out.println("Sum of elements integer array is " + getSumOfIntArr(intArr));
+        System.out.println("Calculation of division is " + getDivision(getSumOfIntArr(intArr)));
     }
 }
