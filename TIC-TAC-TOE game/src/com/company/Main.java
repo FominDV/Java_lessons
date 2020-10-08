5e8a906d0e0f9b8bf4e5e8956351f1796c916e1f
Dmitriy Fomin <79067773397@yandex.ru>
2020-10-08 17:23:36 +0300

    1

---
+++ TIC-TAC-TOE game/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    static GameWindow window;
+
+    public static void main(String[] args) {
+        window = new GameWindow();
+    }
+
+    static void newGame() {
+        window.setVisible(false);
+        window = new GameWindow();
+    }
+}
