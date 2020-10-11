b8b0b8a0dd05646cd9da8d71336eb86073803764
Dmitriy Fomin <79067773397@yandex.ru>
2020-10-11 09:26:54 +0300

    homework2-5.3

--- homework2-3/src/homework2_5/HomeWork2_5.java
+++ homework2-3/src/homework2_5/HomeWork2_5.java
@@ -10,21 +10,22 @@ public class HomeWork2_5 {
         long timeOfFirstMethod, timeOfSecondMethod;
         float[] arr = generateArray();
         timeOfFirstMethod = firstMethod(arr);
+        arr = generateArray();
         timeOfSecondMethod = secondMethod(arr);
         System.out.println("Time difference is " + Math.abs(timeOfFirstMethod - timeOfSecondMethod) + "milliseconds");
     }
 
     private static float[] generateArray() {
         float[] arr = new float[size];
-        for (int i = 0; i < arr.length; i++) arr[i] = 1F;
+        for (int i = 0; i < arr.length; i++) arr[i] = 1f;
         return arr;
     }
 
-    protected static float[] calculatedValuesOfArray(float[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    protected static float[] calculatedValuesOfArray(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        return arr;
+        return array;
     }
 
     private static float[] firstPartOfArr(float[] arr) {
@@ -46,26 +47,31 @@ public class HomeWork2_5 {
 
     private static long secondMethod(float[] startArray) {
         long startTime = System.currentTimeMillis();
-        long timeAfterDivision, timeAfterCalculationFirstParts, timeAfterCalculationSecondParts, timeAfterAddition;
+        long timeAfterDivision, timeAfterCalculation, timeAfterAddition;
         float[] firstPart = firstPartOfArr(startArray);
         float[] secondPart = secondPartOfArr(startArray);
         timeAfterDivision = System.currentTimeMillis();
         SpecialThread t1 = new SpecialThread(firstPart);
         t1.start();
-        float[] firstPartNew = t1.arr;
-        timeAfterCalculationFirstParts = System.currentTimeMillis();
         SpecialThread t2 = new SpecialThread(secondPart);
         t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        float[] firstPartNew = t1.arr;
         float[] secondPartNew = t2.arr;
-        timeAfterCalculationSecondParts = System.currentTimeMillis();
+        timeAfterCalculation = System.currentTimeMillis();
         System.arraycopy(firstPartNew, 0, startArray, 0, h);
         System.arraycopy(secondPartNew, 0, startArray, h, h);
         timeAfterAddition = System.currentTimeMillis();
         System.out.println("Second method:");
         System.out.println("Elapsed Time of the division of array is " + (timeAfterDivision - startTime) + " milliseconds.");
-        System.out.println("Elapsed Time of the calculations a new values for the first part is " + (timeAfterCalculationFirstParts - timeAfterDivision) + " milliseconds.");
-        System.out.println("Elapsed Time of the calculations a new values for the second part is " + (timeAfterCalculationSecondParts - timeAfterCalculationFirstParts) + " milliseconds.");
-        System.out.println("Elapsed Time of the addition into a one array is " + (timeAfterAddition - timeAfterCalculationSecondParts) + " milliseconds.");
+        System.out.println("Elapsed Time of the calculations a new values for the first part is " + t1.deltaTime + " milliseconds.");
+        System.out.println("Elapsed Time of the calculations a new values for the second part is " + t2.deltaTime + " milliseconds.");
+        System.out.println("Elapsed Time of the addition into a one array is " + (timeAfterAddition - timeAfterCalculation) + " milliseconds.");
         System.out.println("Elapsed Time of the second method is " + (timeAfterAddition - startTime) + " milliseconds.");
         return timeAfterAddition - startTime;
     }
--- homework2-3/src/homework2_5/SpecialThread.java
+++ homework2-3/src/homework2_5/SpecialThread.java
@@ -2,12 +2,19 @@ package homework2_5;
 
 public class SpecialThread extends Thread {
     protected float[] arr;
+    protected long startTime;
+    protected long deltaTime;
 
-    public SpecialThread(float[] arr) {
-        this.arr = arr;
+    public SpecialThread(float[] arr){
+        this.arr=arr;
     }
-
-    public SpecialThread(Runnable target) {
-        arr = HomeWork2_5.calculatedValuesOfArray(arr);
+    @Override
+    public void run() {
+        startTime=System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            deltaTime = System.currentTimeMillis() - startTime;
+        }
+        deltaTime = System.currentTimeMillis() - startTime;
     }
 }
