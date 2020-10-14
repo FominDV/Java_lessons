ab36c59c1c7e2ab22105c86f63aed91955450b88
Dmitriy Fomin <79067773397@yandex.ru>
2020-10-14 09:58:43 +0300

    3

--- homework2-6/chat-client/src/ru/geekbrains/java_two/chat/client/CreateSomeClientGUI.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.java_two.chat.client;
-
-public class CreateSomeClientGUI {
-    public static void main(String [] args){
-        for(int i=0; i<3; i++) new ClientGUI();
-    }
-}
